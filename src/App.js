8e10963f7fb0611ef72e1fcd4e6bd47c5885305f
林柏仲 <lbzz1906@qq.com>
2022-03-08 16:00:26 +0800

    基本框架搭建

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <p>5</p>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import Demo from "./pages/Demo";
+const App = () => {
+  return <Demo></Demo>;
+};
+
+export default App;
